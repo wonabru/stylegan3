c0231a4919dec4866c148f4834aa96a3231fa633
wonabru <wonabru@gmail.com>
2023-07-01 21:04:17 +0200

    Update networks_stylegan2.py

--- training/networks_stylegan2.py
+++ training/networks_stylegan2.py
@@ -235,10 +235,11 @@ class MappingNetwork(torch.nn.Module):
             if self.z_dim > 0:
                 misc.assert_shape(z, [None, self.z_dim])
                 x = normalize_2nd_moment(z.to(torch.float32))
-            if self.c_dim > 0:
-                misc.assert_shape(c, [None, self.c_dim])
-                y = normalize_2nd_moment(self.embed(c.to(torch.float32)))
-                x = torch.cat([x, y], dim=1) if x is not None else y
+            # if self.c_dim > 0:
+            #     misc.assert_shape(c, [None, self.c_dim])
+            #     y = normalize_2nd_moment(self.embed(c.to(torch.float32)))
+            #     x = torch.cat([x, y], dim=1) if x is not None else y
+            x = torch.cat([x, torch.zeros_like(x)], dim=1)
 
         # Main layers.
         for idx in range(self.num_layers):
