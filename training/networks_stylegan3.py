f85ef9d0295cfdb41d52d7dfce2f1f509ab835e6
wonabru <wonabru@gmail.com>
2023-07-02 22:17:36 +0200

    Update networks_stylegan3.py

--- training/networks_stylegan3.py
+++ training/networks_stylegan3.py
@@ -151,8 +151,8 @@ class MappingNetwork(torch.nn.Module):
         #     misc.assert_shape(c, [None, self.c_dim])
         #     y = self.embed(c.to(torch.float32))
         #     y = y * (y.square().mean(1, keepdim=True) + 1e-8).rsqrt()
-            x = x.expand(512,x.shape[1]) if x is not None else torch.ones((512, 512)).to(device)
-            x = x.expend(512, 512).to(device)
+            x = x.expand(512,-1) if x is not None else torch.ones((512, 512)).to(device)
+            x = x.expend(-1, 512).to(device)
 
         # Execute layers.
         for idx in range(self.num_layers):
